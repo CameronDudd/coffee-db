cd62400b03f082b7d0c7062b155b2dabb2388cd1
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2025-07-18 17:54:13 +0100

    feat(coffee-diary-schema): minor changes, links, setup

--- coffee-diary-schema.sql
+++ coffee-diary-schema.sql
@@ -36,29 +36,51 @@ CREATE TABLE IF NOT EXISTS brewing_methods (
     name TEXT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS vendors (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    website TEXT,
+    phone TEXT
+);
+
 CREATE TABLE IF NOT EXISTS beans (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     roaster TEXT,
     origin TEXT,
+    vendor_id INTEGER,
     roast_time INTEGER CHECK (roast_time >= 0),  -- Unix time seconds
     roast_level INTEGER CHECK (roast_level BETWEEN 0 AND 5),
-    process TEXT,
-    notes TEXT
+    process TEXT,  -- washed? Maybe switch to boolean
+    notes TEXT,
+    FOREIGN KEY (vendor_id) REFERENCES vendors(id)
 );
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS vendor_locations (
     id INTEGER PRIMARY KEY,
-    username TEXT NOT NULL,
-    name TEXT,
-    email TEXT
-);
-
-CREATE TABLE IF NOT EXISTS locations (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
+    vendor_id INTEGER NOT NULL,
     lat FLOAT,
-    lon FLOAT
+    lon FLOAT,
+    FOREIGN KEY (vendor_id) REFERENCES vendors(id)
+);
+
+CREATE TABLE IF NOT EXISTS cafes (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cafe_locations (
+    id INTEGER PRIMARY KEY,
+    cafe_id INTEGER NOT NULL,
+    lat FLOAT,
+    lon FLOAT,
+    FOREIGN KEY (cafe_id) REFERENCES cafes(id)
 );
 
 CREATE TABLE IF NOT EXISTS brew_sessions (
@@ -66,12 +88,20 @@ CREATE TABLE IF NOT EXISTS brew_sessions (
     dose_grams FLOAT,
     yield_grams FLOAT,
     pressure_bar FLOAT,
-    brew_time INTEGER NOT NULL CHECK (brew_time >= 0),  -- Unix time seconds
+    brew_time INTEGER CHECK (brew_time IS NULL OR brew_time >= 0),
     rating INTEGER NOT NULL CHECK (rating BETWEEN 0 AND 10),
-    bean_id INTEGER NOT NULL,
-    grind_setting_id INTEGER NOT NULL,
-    machine_id INTEGER NOT NULL,
+    date INTEGER NOT NULL CHECK (date >= 0),  -- Unix time seconds
+    bean_id INTEGER,
+    grind_setting_id INTEGER,
+    brewing_method_id INTEGER,
+    user_id INTEGER,
+    machine_id INTEGER,
+    cafe_location_id INTEGER,
+    notes TEXT,
     FOREIGN KEY (bean_id) REFERENCES beans(id),
     FOREIGN KEY (grind_setting_id) REFERENCES grind_settings(id),
-    FOREIGN KEY (machine_id) REFERENCES machines(id)
+    FOREIGN KEY (brewing_method_id) REFERENCES brewing_methods(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (machine_id) REFERENCES machines(id),
+    FOREIGN KEY (cafe_location_id) REFERENCES cafe_locations(id)
 );
